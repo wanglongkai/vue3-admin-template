d63e0b024087ce57e05f6ad3f821c2df590e204d
wanglongkai <13699283312@163.com>
2023-10-23 10:49:33 +0800

    feat: 测试Vue3父子组件的生命周期流程

---
+++ src/views/home/child.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class='app-container'>
+    子组件{{ count }}
+  </div>
+</template>
+
+<script lang='ts' setup>
+import { onBeforeMount, onBeforeUnmount, onBeforeUpdate, onMounted, onUnmounted, onUpdated } from 'vue';
+defineProps(['count'])
+
+console.log('子组件setup')
+onBeforeMount(() => {
+  console.log('子组件onBeforeMount')
+})
+onMounted(() => {
+  console.log('子组件onMounted')
+})
+onBeforeUpdate(() => {
+  console.log('子组件onBeforeUpdate')
+})
+onUpdated(() => {
+  console.log('子组件onUpdated')
+})
+onBeforeUnmount(() => {
+  console.log('子组件onBeforeUnmount')
+})
+onUnmounted(() => {
+  console.log('子组件onUnmounted')
+})
+</script>
+
+<style scoped lang='scss'>
+</style>
--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -1,13 +1,48 @@
 <template>
   <div class='app-container'>
-    home
+    {{ count }} <el-button @click="count++">++</el-button>
+    <Child :count="count"/>
+    <el-button @click="changeRoute">卸载</el-button>
   </div>
 </template>
 
 <script lang='ts' setup>
 import { getUserInfo } from '@/api/user'
+import Child from './child.vue'
+import { onBeforeMount, onBeforeUnmount, onBeforeUpdate, onMounted, onUnmounted, onUpdated, ref } from 'vue';
+import { useRouter } from 'vue-router'
+
+
+const router = useRouter();
+
+const changeRoute = () => {
+  router.push('/sss')
+}
 
 getUserInfo(1);
+
+const count = ref(0);
+console.log('父组件setup')
+onBeforeMount(() => {
+  console.log('父组件onBeforeMount')
+})
+onMounted(() => {
+  console.log('父组件onMounted')
+})
+onBeforeUpdate(() => {
+  console.log('父组件onBeforeUpdate')
+})
+onUpdated(() => {
+  console.log('父组件onUpdated')
+})
+onBeforeUnmount(() => {
+  console.log('父组件onBeforeUnmount')
+})
+onUnmounted(() => {
+  console.log('父组件onUnmounted')
+})
+
+
 </script>
 
 <style scoped lang='scss'>
