d9a8bb21fd058e28ec5f2ee440a5c36f03661641
wanglongkai <13699283312@163.com>
2023-11-07 15:37:22 +0800

    feat: 在.vue中使用tsx

--- src/views/folder1/index.vue
+++ src/views/folder1/index.vue
@@ -1,10 +1,23 @@
 <template>
   <div class='app-container'>
-    folder1
+    <el-button plain @click="open">Common Tsx</el-button>
   </div>
 </template>
 
-<script lang='ts' setup>
+<script lang='tsx' setup>
+import { ElMessageBox} from 'element-plus'
+import { ref } from 'vue';
+
+const num = ref(0);
+
+const open = () => {
+  ElMessageBox({
+    title: 'Message',
+    message:() => <p>我是一段tsx-- <el-button onClick={() => num.value++}>{ num.value }</el-button></p>,
+  }).catch(err=> {
+    console.log(err, 19)
+  })
+}
 </script>
 
 <style scoped lang='scss'>
